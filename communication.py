2a7c966aa6965db0f8e8af24f09d3e0e65d4cbec
fra2-ui <francesco2.cazzaniga@mail.polimi.it>
2026-04-21 15:52:42 +0200

    Update all files

--- communication.py
+++ communication.py
@@ -22,35 +22,41 @@ def send_package(port, ID_message, data):
     header = b'\xAA'
     ID_message_bytes = bytes([ID_message])
     data_length = bytes([len(data)])
-    data_bytes = bytes(data)
     CRC = crc8(ID_message_bytes + data_length + data_bytes)
     CRC_byte = bytes([CRC])
     package = header + ID_message_bytes + data_length + data_bytes + CRC_byte
-    print(f"DEBUG: {package.hex().upper()}")
     port.write(package)
 
 def receive_package(port):
     while True:
         header = port.read(1)
-        if header == b'\xaa':
+        if header == b'\xAA':
             ID_message_bytes = port.read(1)
             data_length_bytes = port.read(1)
             ID_message = ID_message_bytes[0]
             data_length = data_length_bytes[0]
             if not ID_message_bytes or not data_length_bytes: continue
-            payload = port.read(data_length)
-            if len(payload) != data_length: continue
+            payload_bytes = port.read(data_length)
+            if (len(payload_bytes) != data_length): continue
             crc_bytes = port.read(1)
             if not crc_bytes: continue
-            CRC = crc8(ID_message + data_length + payload)
+            CRC = crc8(ID_message_bytes + data_length_bytes + payload_bytes)
             if CRC == crc_bytes[0]:
                 if ID_message == 0xFA:
-                    print("ACK")
                     ack = True
+                    print(f"ACK: {ack}")
                 elif ID_message == 0xFB:
-                    print("NACK")
                     ack = False
+                    print(f"ACK: {ack}")
                 else:
+                    print(f"header: {header.hex().upper()}, Received message ID: {ID_message_bytes.hex().upper()}, Data length: {data_length_bytes.hex().upper()}, Data: {payload_bytes.hex().upper()}")
                     send_package(port, 0xFA, [])
             else:
                 send_package(port, 0xFB, [])
+                print("CRC error")
+
+if __name__ == "__main__":
+    try:
+        receive_package(port)
+    except KeyboardInterrupt:
+        port.close()
