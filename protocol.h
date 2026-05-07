718b4ae27a1b6772f01d2c1aeab2bafc4ebf21ed
fra2-ui <francesco2.cazzaniga@mail.polimi.it>
2026-05-07 16:48:54 +0200

    Update Protocol.h

--- protocol.h
+++ protocol.h
@@ -4,6 +4,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// --- HEADER DEL PACCHETTO ---
+#define PACKET_HEADER 0xAA
+
+// --- STATI DEL PROGRAMMA ---
 typedef enum {
     P_STOP            = 0,
     P_PRINCIPAL       = 1,
@@ -11,7 +15,6 @@ typedef enum {
     P_NOW_CALIBRATION = 3,
     P_NOW_ABSORBANCE  = 4
 } ProgramState;
-
 typedef enum {
     // Program (GUI -> STM32)
     M_PROGRAM          = 0x01,
@@ -39,5 +42,40 @@ typedef enum {
     M_ACK              = 0xFA,
     M_NACK             = 0xFB
 } MessageID;
+
+static inline uint8_t getExpectedPayloadLength(MessageID id) {
+    switch(id) {
+        // Formato: <i (int32_t) -> 4 byte
+        case M_PROGRAM:          return 4; 
+        
+        // Formato: <f (float 32-bit) -> 4 byte
+        case M_RATIO_DD:         return 4; 
+        case M_FLUID_VELOCITY:   return 4; 
+        
+        // Formato: <? (bool) -> 1 byte
+        case M_PUMP_1:           return 1; 
+        case M_PUMP_2:           return 1; 
+        
+        // Formato: <? (bool) -> 1 byte
+        case M_LED_UV:           return 1; 
+        case M_LED_BLU:          return 1; 
+        case M_LED_RED:          return 1; 
+        
+        // Formato: <f (float 32-bit) -> 4 byte
+        case M_ABS_UV:           return 4; 
+        case M_ABS_BLU:          return 4; 
+        case M_ABS_RED:          return 4; 
+        
+        // Formato: <f (float 32-bit) -> 4 byte
+        case M_RATE:             return 4; 
+        
+        // Nessun payload -> 0 byte
+        case M_ACK:              return 0; 
+        case M_NACK:             return 0; 
+        
+        // Messaggio sconosciuto
+        default:                 return 0; 
+    }
+}
 
 #endif
